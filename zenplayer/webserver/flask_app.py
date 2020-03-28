3f6a45bab707df34c7ce64eada8de0098b9d45b7
Zen-CODE <zenkey.zencode@gmail.com>
2020-03-28 15:37:27 +0200

    :arrow_up: Added play/pause as first endpoint

--- zenplayer/webserver/flask_app.py
+++ zenplayer/webserver/flask_app.py
@@ -11,6 +11,15 @@ def index():
 
 @app.route('/play_pause')
 def play_pause():
+    """
+    Play or pause the currently active player.
+    ---
+    tags:
+        - ZenPlayer
+    responses:
+        200:
+            description: Success if we have played or paused the current player.
+    """
     return "play_pause"
 
 
