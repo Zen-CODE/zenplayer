935769b63549d01c9f8006b3262a323526de2ccb
Zen-CODE <zenkey.zencode@gmail.com>
2020-04-02 20:22:51 +0200

    :lipstick: Pep 8 fixes to doc strings

--- zenplayer/ui/playlistrv.py
+++ zenplayer/ui/playlistrv.py
@@ -12,30 +12,30 @@ from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 
 class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior,
                                  RecycleBoxLayout):
-    ''' Adds selection and focus behaviour to the view. '''
+    """ Adds selection and focus behaviour to the view. """
 
 
 class SelectableLabel(RecycleDataViewBehavior, Label):
-    ''' Add selection support to the Label '''
+    """ Add selection support to the Label """
     index = None
     selected = BooleanProperty(False)
     selectable = BooleanProperty(True)
 
     def refresh_view_attrs(self, rv, index, data):
-        ''' Catch and handle the view changes '''
+        """ Catch and handle the view changes """
         self.index = index
         return super(SelectableLabel, self).refresh_view_attrs(
             rv, index, data)
 
     def on_touch_down(self, touch):
-        ''' Add selection on touch down '''
+        """ Add selection on touch down """
         if super(SelectableLabel, self).on_touch_down(touch):
             return True
         if self.collide_point(*touch.pos) and self.selectable:
             return self.parent.select_with_touch(self.index, touch)
 
     def apply_selection(self, rv, index, is_selected):
-        ''' Respond to the selection of items in the view. '''
+        """ Respond to the selection of items in the view. """
         self.selected = is_selected
         if is_selected:
             print("selection changed to {0}".format(rv.data[index]))
