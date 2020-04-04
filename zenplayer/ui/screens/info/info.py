1bdce4b596d259f08c9ad4c7d18c90273b4ae8de
Zen-CODE <zenkey.zencode@gmail.com>
2020-04-04 19:21:52 +0200

    :art: Added track info extraction

--- zenplayer/ui/screens/info/info.py
+++ zenplayer/ui/screens/info/info.py
@@ -4,6 +4,7 @@ This module houses the screen displaying track information
 from kivy.uix.screenmanager import Screen
 from kivy.properties import ObjectProperty, StringProperty
 from kivy.lang import Builder
+from kivy.clock import Clock
 
 
 class InfoScreen(Screen):
@@ -18,3 +19,18 @@ class InfoScreen(Screen):
         self.ctrl = ctrl
         Builder.load_file("ui/screens/info/info.kv")
         super(InfoScreen, self).__init__(**kwargs)
+
+    def on_filename(self, _widget, filename):
+        """ Respond to the changing of the filename """
+        Clock.schedule_once(lambda dt: self._show_info(filename))
+
+    def _show_info(self, filename):
+        """ Populate the track info """
+        data = self.ctrl.playlist.get_info(filename=filename)
+        for key in data.keys():
+            self.ids[key].text = data[key]
+        self.ids["filename"] = filename
+
+    def _show_details(self, filename):
+        """ Populate the track info """
+        pass
