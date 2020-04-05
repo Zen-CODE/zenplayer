81fa2bd0c0b86090bceaffb7dead74df821e983e
Zen-CODE <zenkey.zencode@gmail.com>
2020-04-05 17:02:06 +0200

    :recycle: Removed redundant printing

--- zenplayer/ui/screens/playlist/playlistrv.py
+++ zenplayer/ui/screens/playlist/playlistrv.py
@@ -49,10 +49,6 @@ class SelectableLabel(RecycleDataViewBehavior, Label):
     def apply_selection(self, rv, index, is_selected):
         """ Respond to the selection of items in the view. """
         self.selected = is_selected
-        if is_selected:
-            print("selection changed to {0}".format(rv.data[index]))
-        else:
-            print("selection removed for {0}".format(rv.data[index]))
 
 
 class PlaylistRV(RecycleView):
