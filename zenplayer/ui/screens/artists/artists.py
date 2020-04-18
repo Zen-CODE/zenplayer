433ef3595d0a78c1903d2e27a369df3bdd545031
Zen-CODE <zenkey.zencode@gmail.com>
2020-04-18 15:11:42 +0200

    :bookmarek: Started with notifications

--- zenplayer/ui/screens/artists/artists.py
+++ zenplayer/ui/screens/artists/artists.py
@@ -2,12 +2,26 @@
 This module houses the Artist display UI
 """
 from ui.screens.zenscreen import ZenScreen
+from kivy.properties import StringProperty, BooleanProperty
+from kivy.animation import Animation
 
 
 class ArtistsScreen(ZenScreen):
     """
     Displays a interface for viewing and interacting with the artists
     list from the library.
+    """
+
+    note_text = StringProperty("Loading library. Please wait..")
+    """
+    The text, obtained from the keyboard, for searched for items in the list
+    of atists.
+    """
+
+    show_note = BooleanProperty(True)
+    """
+    Boolean property dictating whether or not the notification label should
+    be dispalyed.
     """
 
     def on_enter(self):
@@ -18,8 +32,7 @@ class ArtistsScreen(ZenScreen):
         if not self.ids.rv.data:
             self.ids.rv.data = [
                 {"text": artist} for artist in self.ctrl.library.get_artists()]
-            lbl = self.ids.wait_label
-            lbl.parent.remove_widget(lbl)
+            self.note_text = ""
 
     def item_selected(self, label, selected):
         """
@@ -27,3 +40,12 @@ class ArtistsScreen(ZenScreen):
         """
         if selected:
             self.ctrl.show_screen("Albums", artist=label.text)
+
+    def on_show_note(self, widget, value):
+        """ Either hide of show the note label """
+        end_vale = 1 if value else 0
+        Animation(opacity=end_vale, duration=1).start(self.ids.note_label)
+
+    def on_note_text(self, widget, text):
+        """ Handle the change of note text """
+        self.show_note = bool(text)
