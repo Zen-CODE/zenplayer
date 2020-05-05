3f0eb746feda5ce5b8acd916b4a864eaf91b6a9b
Zen-CODE <zenkey.zencode@gmail.com>
2020-05-05 22:33:30 +0200

    -Tweaked launch script

--- run_in_env.sh
+++ run_in_env.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 echo "Changing to the ZenPlayer folder and launching."
-echo "sudo is needed for hotkey support"
-echo "================================="
 cd "${0%/*}"
 KIVY_AUDIO=vlcplayer venv/bin/python zenplayer/main.py
