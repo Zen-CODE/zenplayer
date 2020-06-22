b840c3279b0f14b7a30a0bc46a2959b35010155d
Zen-CODE <zenkey.zencode@gmail.com>
2020-06-22 19:21:15 +0200

    :construction: Start with context screen

---
+++ zenplayer/ui/screens/context/context.py
@@ -0,0 +1,21 @@
+"""
+This module houses the Zen Music Library browser based on the recycleview
+"""
+from ui.screens.zenscreen import ZenScreen
+from kivy.properties import StringProperty, ListProperty
+from ui.widgets.zenkeydown import ZenKeyDown
+
+
+class ContextScreen(ZenKeyDown, ZenScreen):
+    """
+    Displays a interface for viewing and interacting with the `Library`
+    component
+    """
+    title = StringProperty("Context title")
+    """ The artist for which to display the ALbum """
+
+    actions = ListProperty()
+    """ A list or dicts, with *text* and *action properties, where the *text*
+    is what is displayed and the *action* the function called if that text
+    is selected."""
+
