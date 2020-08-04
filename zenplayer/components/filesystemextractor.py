19a5e7a86e9efc872754d00d7b4653c950f44920
fruitbat <zenkey.zencode@gmail.com>
2020-08-04 22:09:51 +0200

    :construction: Added type change

--- zenplayer/components/filesystemextractor.py
+++ zenplayer/components/filesystemextractor.py
@@ -1,1 +1,49 @@
-/home/fruitbat/Repos/zen-code/zenprojects/python/fastapi/library/app/components/filesystemextractor.py
+"""
+This module houses the FileSystemExtractor class
+"""
+from os import listdir
+from os.path import isdir, join
+
+
+class FileSystemExtractor:
+    """
+    Helper class for extracting tracks and albums covers from
+    folders
+    """
+    music_types = [".ogg", ".mp3", ".wma"]
+    art_types = [".jpg", ".jpeg", ".png", ".gif"]
+
+    @staticmethod
+    def get_dirs(folder):
+        """
+        Return a list of sorted sub-folders.
+
+        Args:
+            folder (str): The path to the folder to process
+        Returns:
+            (list): A sorted list of folder in the specified folder.
+        """
+        return [f for f in sorted(listdir(folder))
+                if isdir(join(folder, f))]
+
+    @staticmethod
+    def get_media(folder):
+        """
+        Return a lists of tracks and covers in the given folder.
+
+        Args:
+            folder (str): The path to the folder to process
+        Returns:
+            (list), (list): A sorted list of music tracks and album images
+                            respectively.
+
+
+        """
+        tracks, covers, fse = [], [], FileSystemExtractor
+        for item in sorted(listdir(folder)):
+            if any([item.endswith(e) for e in fse.music_types]):
+                tracks.append(item)
+            elif any([item.endswith(e) for e in fse.art_types]):
+                covers.append(item)
+
+        return tracks, covers
