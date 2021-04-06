2014d5e7b12045802880a48a638280cebb26a8cb
Richard Larkin <zenkey.zencode@gmail.com>
2021-04-06 19:23:26 +0200

    :arrow_up: Updated to kivy 2, leap launch compatibility

--- run_in_env.sh
+++ run_in_env.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "Changing to the ZenPlayer folder and launching."
-. venv/bin/activate
-KIVY_AUDIO=vlcplayer python zenplayer/main.py
+KIVY_AUDIO=vlcplayer LD_PRELOAD=./zenplayer/libLeap.so python zenplayer/main.py
