1230fb8fe55175ff86d2d24c341188f79e687ce2
Richard Larkin <zenkey.zencode@gmail.com>
2022-08-28 08:17:28 +0200

    :art: Extend theming, default light color

--- zenplayer/ui/styles.py
+++ zenplayer/ui/styles.py
@@ -1,3 +1,4 @@
 """This module houses styling configuration."""
-BACK_COLOR = [0.1, 0.1, 0.1, 0]
-FONT_COLOR = [1.0, 1.0, 0, 0.8]
+BACK_COLOR = [0.95, 0.95, 0.95, 0]
+FONT_COLOR = [0.1, 0.1, 0.1, 0.8]
+HEADER_FONT_COLOR = [0.0, .75, 0.0, 1.0]
