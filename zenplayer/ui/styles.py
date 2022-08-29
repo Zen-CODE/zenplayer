137bfcb1ede1a8cd3e85a7f96e9a9b1508aedd5a
Richard Larkin <zenkey.zencode@gmail.com>
2022-08-29 20:01:42 +0200

    :recycle: Revert to black

--- zenplayer/ui/styles.py
+++ zenplayer/ui/styles.py
@@ -1,5 +1,12 @@
 """This module houses styling configuration."""
-BACK_COLOR = [0.95, 0.95, 0.95, 0]
-FONT_COLOR = [0.1, 0.1, 0.1, 0.8]
-HEADER_FONT_COLOR = [0.0, .75, 0.0, 1.0]
-BUTTON_FONT_COLOR = [0.9, 0.9, 0.9, 1]
+# DARK THEME
+BACK_COLOR = [0.1, 0.1, 0.1, 0]
+FONT_COLOR = [0.95, 0.95, 0.95, 0.8]
+HEADER_FONT_COLOR = [0.95, .5, 0.95, 1.0]
+BUTTON_FONT_COLOR = [0.1, 0.1, 0.1, 1]
+
+# Light theme
+# BACK_COLOR = [0.95, 0.95, 0.95, 0]
+# FONT_COLOR = [0.1, 0.1, 0.1, 0.8]
+# HEADER_FONT_COLOR = [0.0, .75, 0.0, 1.0]
+# BUTTON_FONT_COLOR = [0.9, 0.9, 0.9, 1]
