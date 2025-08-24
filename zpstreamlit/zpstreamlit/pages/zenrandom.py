d6c2e5005c6cfba98ad3a6e7e2ba6a3ed0707e05
Richard Larkin <zenkey.zencode@gmail.com>
2025-08-24 11:23:55 +0200

    Random working

--- zpstreamlit/zpstreamlit/pages/zenrandom.py
+++ zpstreamlit/zpstreamlit/pages/zenrandom.py
@@ -1,23 +1,14 @@
 import streamlit as st
-# from .zencore import ZENPLAYER_URL
-# import requests
+from .zencore import ZENPLAYER_URL
+import requests
+from html import escape
 
 
 def get_zenrandom():
-    def get_time(time_s):
-        return str(int(time_s // 60)).zfill(2) + "m " + \
-            str(int(time_s % 60)).zfill(2) + "s"
 
-    st.markdown("**Random Album**")
-    # data = requests.get(f"{ZENPLAYER_URL}/zenplayer/get_state").json()
-    # meta = requests.get(f"{ZENPLAYER_URL}/zenplayer/get_track_meta").json()
-
-    # st.markdown(f"**{data['artist']}: {data['album']}** - " \
-    #                 f"*{data['file_name'].split('/')[-1].split('.')[0]}*")
-    # st.write(
-    #     f"{meta['sample_rate']}hz, {meta['bitrate']}kbps, {get_time(meta['length'])}")
-    # st.image(f"{ZENPLAYER_URL}/zenplayer/get_track_cover",
-    #             use_container_width=True)
-    # zp.write(streamlit_image_coordinates(
-    #     f"{ZENPLAYER_URL}/zenplayer/get_track_cover"),
-    #     use_column_width="always")
+    random = requests.get(f"{ZENPLAYER_URL}/zenlibrary/get_random_album").json()
+    st.markdown(f"**Artist:** {random['artist']}")
+    st.markdown(f"**Album:** {random['album']}" )
+    url = f"{ZENPLAYER_URL}/zenlibrary/get_album_cover?" \
+          f"artist={escape(random['artist'])}&album={escape(random['album'])}"
+    st.image(url, use_container_width=True)
