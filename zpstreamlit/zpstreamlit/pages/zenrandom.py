1a617ac5dcb7d96324b6b88ea9035c698bf71b17
Richard Larkin <zenkey.zencode@gmail.com>
2025-08-24 11:51:04 +0200

    Add rnadom panel;

--- zpstreamlit/zpstreamlit/pages/zenrandom.py
+++ zpstreamlit/zpstreamlit/pages/zenrandom.py
@@ -12,3 +12,33 @@ def get_zenrandom():
     url = f"{ZENPLAYER_URL}/zenlibrary/get_album_cover?" \
           f"artist={escape(random['artist'])}&album={escape(random['album'])}"
     st.image(url, use_container_width=True)
+
+    class Buttons:
+        @staticmethod
+        def click(action=None, artist=None, album=None):
+            if action:
+                requests.get(f"{ZENPLAYER_URL}/zenplaylist/add_files?" \
+                            f"{escape(random['artist'])}/{escape(random['album'])}")
+            else:
+                st.rerun()
+
+    button_width = 80
+    add_, replace_, insert_, next_, next_album_ = st.columns(
+        spec=[1, 1, 1, 1, 1], border=True)
+
+    add_.button("Add",
+                on_click=Buttons.click, args=("add", random["artist"], random["album"]),
+                width=button_width)
+    replace_.button("Replace",
+                on_click=Buttons.click, args=("replace", random["artist"], random["album"]),
+                width=button_width)
+    insert_.button("Insert",
+                on_click=Buttons.click, args=("insert", random["artist"], random["album"]),
+                width=button_width)
+    next_.button("Next",
+                on_click=Buttons.click, args=("next", random["artist"], random["album"]),
+                width=button_width)
+    next_album_.button("Another",
+                on_click=Buttons.click,
+                width=button_width)
+
