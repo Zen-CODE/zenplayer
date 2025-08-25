2b9a415f9ebdb90467fcc47cacf1946c28204ac7
Richard Larkin <zenkey.zencode@gmail.com>
2025-08-25 20:18:05 +0200

    Add artist info

--- zpstreamlit/zpstreamlit/main.py
+++ zpstreamlit/zpstreamlit/main.py
@@ -1,11 +1,13 @@
 import streamlit as st
 from pages.zenplayer import get_zenplayer
 from pages.zenrandom import get_zenrandom
+from pages.zeninfo import get_zeninfo
 
 pages = {
     "ZenPlayer": [
         st.Page(get_zenplayer, title="Now Playing", icon=":material/play_circle:"),
-        st.Page(get_zenrandom, title="Random Album", icon=":material/shuffle_on:")
+        st.Page(get_zenrandom, title="Random Album", icon=":material/shuffle_on:"),
+        st.Page(get_zeninfo, title="Artist Info", icon=":material/info:")
     ]
 }
 
---
+++ zpstreamlit/zpstreamlit/pages/zeninfo.py
@@ -0,0 +1,21 @@
+import wikipediaapi
+from zencore import ZENPLAYER_URL
+import requests
+import streamlit as st
+
+
+
+def get_zeninfo():
+    """Show  info about the urrently playing artist"""
+    data = requests.get(f"{ZENPLAYER_URL}/zenplayer/get_state").json()
+    wiki_wiki = wikipediaapi.Wikipedia('ZenPlayer (zenkey.zencode@gmail.com)', 'en')
+    page = wiki_wiki.page(data['artist'])
+    if page.exists():
+        st.markdown(f"# Artist: {data['artist']}")
+        st.markdown("## Summary")
+        st.write(page.summary)
+        st.markdown("## Full Text")
+        st.write(page.text)
+
+    else:
+        st.write(f"No information found for *{data['artist']}*")
