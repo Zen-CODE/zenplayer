7574d0b64319f6c058e6ae318fbe5faa3519de69
Richard Larkin <zenkey.zencode@gmail.com>
2025-11-15 10:16:24 +0200

    Started with buttons

--- zpstreamlit/zpstreamlit/app_pages/sysinfo.py
+++ zpstreamlit/zpstreamlit/app_pages/sysinfo.py
@@ -4,23 +4,43 @@ from datetime import datetime
 from time import time, sleep
 
 
-def show_cpu():
+def show_cpu(container):
     """Display the CPU times as a progress bar"""
     perc = cpu_percent()
-    st.progress(perc / 100.0, f"CPU: {int(perc)}%")
+    container.progress(perc / 100.0, f"CPU: {int(perc)}%")
 
 
-def show_memory():
+def show_memory(container):
     """Display the memory usage as a progress bar"""
     memory = virtual_memory()
     perc = memory.percent
-    st.progress(
+    container.progress(
         perc / 100.0,
         f"Memory: {int(perc)}% of {int(float(memory.total) / (1024**3))}GB",
     )
 
 
-def show_start_time():
+def show_buttons(container):
+    """Adds a row of control buttons for system control."""
+    button_width = 80
+    suspend_, restart_, shutdown_ = container.columns(spec=[1, 1, 1], border=True)
+    suspend_.button(
+        "Suspend",
+        on_click=lambda *args: print("Suspend"),
+        args=("play_previous",),
+        width=button_width,
+    )
+    restart_.button(
+        "Restart", on_click=lambda *args: print("Restart"), width=button_width
+    )
+    shutdown_.button(
+        "Shutdown",
+        on_click=lambda *args: print("Shutdown"),
+        width=button_width,
+    )
+
+
+def show_start_time(container):
     """Show the system startup time."""
     start_time = boot_time()
     display_time = datetime.fromtimestamp(start_time).strftime("%Y-%m-%d, %H:%M")
@@ -28,21 +48,22 @@ def show_start_time():
 
     days = int(time_diff / (60 * 60 * 24))
     hours = int((time_diff / (60 * 60)) % 24)
-    st.markdown(f"*Started:* {display_time}. *Uptime:* {days}d, {hours}h")
+    container.markdown(f"*Started:* {display_time}. *Uptime:* {days}d, {hours}h")
 
 
 def get_sysinfo():
     """Show information and sleep, shutdown and restart buttons."""
-    st.empty()
-    st.image("images/cpu.jpg", width=128)
-    st.markdown("# System Information")
+    container = st.container()
+    container.empty()
+    container.image("images/cpu.jpg", width=128)
+    container.markdown("# System Information")
 
-    show_cpu()
-    show_memory()
-    show_start_time()
+    show_cpu(container)
+    show_memory(container)
+    show_start_time(container)
+    show_buttons(container)
 
-    placeholder = st.empty()
     while True:
-        with placeholder.container():
+        with container:
             sleep(1)
             st.rerun()
