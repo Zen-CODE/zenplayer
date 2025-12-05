29bf824d1f363a59e356f3f03edc30ee78a25ba5
Richard Larkin <zenkey.zencode@gmail.com>
2025-12-05 18:19:47 +0200

    Fix dict issue

--- zenstream/handlers/audioplayer.py
+++ zenstream/handlers/audioplayer.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from pathlib import Path
 from mutagen import File
 from glob import glob
 from os import sep
@@ -8,22 +7,21 @@ from styler import Styler
 
 
 class AudioPlayer:
-
     @staticmethod
     def _show_meta(file_name: str):
-
         info: File = File(file_name).info
         parts = file_name.split(sep)
-        data = {"Artist", parts[-3],
-                "Album", parts[-2],
-                "Track", parts[-1],
-                "Length", f"{int(info.length // 60)}m {int(info.length % 60)}s",
-                "Bitrate", f"{info.bitrate // 1000}kbps",
-                "Bitrate mode", AudioPlayer._get_bitrate(info),
-                "Channels", info.channels,
-                "Sample_rate", f"{info.sample_rate}hz"}
-        Styler.show("Track Metadata", data)
-
+        data = {
+            "Artist": parts[-3],
+            "Album": parts[-2],
+            "Track": parts[-1],
+            "Length": f"{int(info.length // 60)}m {int(info.length % 60)}s",
+            "Bitrate": f"{info.bitrate // 1000}kbps",
+            "Bitrate mode": AudioPlayer._get_bitrate(info),
+            "Channels": info.channels,
+            "Sample_rate": f"{info.sample_rate}hz",
+        }
+        Styler.show_dict("Track Metadata", data)
 
     @staticmethod
     def show_file(file_name: str):
@@ -35,8 +33,6 @@ class AudioPlayer:
         if file_name.lower().endswith(".mp3"):
             AudioPlayer._show_meta(file_name)
             AudioPlayer._show_cover(file_name)
-
-
 
     @staticmethod
     def _get_bitrate(info_obj: File) -> str:
