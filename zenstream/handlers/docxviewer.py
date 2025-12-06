59f31be884936ff5fe678c8842a17e5224319874
Richard Larkin <zenkey.zencode@gmail.com>
2025-12-06 07:30:19 +0200

    Add doc metadata

--- zenstream/handlers/docxviewer.py
+++ zenstream/handlers/docxviewer.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from styler import Styler
 from docx import Document
+import re
 
 
 class DocXViewer:
@@ -8,8 +9,28 @@ class DocXViewer:
     def show_file(file_name: str):
         st.header("DocX Viewer")
         document = Document(file_name)
+
+        Styler.show_dict(
+            "Metadata",
+            {
+                "Author": document.core_properties.author,
+                "Category": document.core_properties.category,
+                "Comments": document.core_properties.comments,
+                "Status": document.core_properties.content_status,
+                "Created": document.core_properties.created,
+                "Identifier": document.core_properties.identifier,
+                "Keywords": document.core_properties.keywords,
+                "Language": document.core_properties.language,
+                "Last modified by": document.core_properties.last_modified_by,
+                "Revision": document.core_properties.revision,
+                "Subject": document.core_properties.subject,
+                "Title": document.core_properties.title,
+                "Version": document.core_properties.version,
+            },
+        )
+
         paragraph_data = {
-            f"Paragraph {index + 1}": paragraph.text.strip()
+            f"Paragraph {index + 1}": DocXViewer._format_text(paragraph.text)
             for index, paragraph in enumerate(document.paragraphs)
         }
 
@@ -21,6 +42,12 @@ class DocXViewer:
         }
 
         Styler.show_dict("Tables", tables_data)
+
+    @staticmethod
+    def _format_text(text: str) -> str:
+        """Remove non-readable characters"""
+        control_char_pattern = r"[\x00-\x1F\x7F]"
+        return re.sub(control_char_pattern, "", text)
 
     @staticmethod
     def _get_table_rows(table) -> list:
