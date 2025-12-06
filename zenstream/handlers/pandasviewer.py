f7e927c5744019a0b660edbd77b264974292a4b2
Richard Larkin <zenkey.zencode@gmail.com>
2025-12-06 17:48:41 +0200

    Add error handling to graphing

--- zenstream/handlers/pandasviewer.py
+++ zenstream/handlers/pandasviewer.py
@@ -16,11 +16,15 @@ class PandasViewer:
         with st.expander("CSV graphs"):
             st.markdown("CSV analysis")
             col1, col2 = st.columns(2)
-            with col1:
-                st.markdown("**Area Chart**")
-                st.area_chart(df)
-                st.markdown("**Bar Chart**")
-                st.bar_chart(df)
-            with col2:
-                st.markdown("**Line Chart**")
-                st.line_chart(df)
+            try:
+                with col1:
+                    st.markdown("**Area Chart**")
+                    st.area_chart(df)
+                    st.markdown("**Bar Chart**")
+                    st.bar_chart(df)
+                with col2:
+                    st.markdown("**Line Chart**")
+                    st.line_chart(df)
+            except Exception as e:
+                with col1:
+                    st.error(f"The CSV could not be graphed. It gave the error: {e}")
