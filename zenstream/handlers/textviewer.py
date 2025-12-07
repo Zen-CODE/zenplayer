2f628cee0a88f4ef461327bc32356ee0be948ccd
Richard Larkin <zenkey.zencode@gmail.com>
2025-12-07 17:07:36 +0200

    Move rendered markdown to collapsible

--- zenstream/handlers/textviewer.py
+++ zenstream/handlers/textviewer.py
@@ -9,12 +9,18 @@ class TextViewer:
     def show_file(file_name: str):
         st.header("Text Viewer")
         st.subheader(file_name.split(sep)[-1])
+
         with open(file_name, "r", encoding="utf-8") as f:
             lines = f.read()
 
         match file_name.split(".")[-1].lower():
             case "md":
-                st.markdown(lines)
+                st.code(
+                    lines,
+                    line_numbers=True,
+                )
+                with st.expander("Rendered markdown"):
+                    st.markdown(lines)
             case "json":
                 st.json(lines)
                 df = pd.read_json(file_name)
