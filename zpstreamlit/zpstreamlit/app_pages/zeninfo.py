e0239a9566d126bcdf677a34c167d86c484e8c81
Richard Larkin <zenkey.zencode@gmail.com>
2025-12-07 19:14:31 +0200

    Add caching to wikipedia retrieval

--- zpstreamlit/zpstreamlit/app_pages/zeninfo.py
+++ zpstreamlit/zpstreamlit/app_pages/zeninfo.py
@@ -5,13 +5,19 @@ import streamlit as st
 from styler import Styler
 
 
+@st.cache_data
+def get_artist_data(artist):
+    print("ZenInfo: Fetching wikipedia data...")
+    wiki_wiki = wikipediaapi.Wikipedia("ZenPlayer (zenkey.zencode@gmail.com)", "en")
+    page = wiki_wiki.page(artist)
+    return page
+
+
 def show_zeninfo():
     """Show  info about the urrently playing artist"""
 
-    print("ZenInfo: Fetching wikipedia data...")
     data = requests.get(f"{ZENPLAYER_URL}/zenplayer/get_state").json()
-    wiki_wiki = wikipediaapi.Wikipedia("ZenPlayer (zenkey.zencode@gmail.com)", "en")
-    page = wiki_wiki.page(data["artist"])
+    page = get_artist_data(data["artist"])
 
     Styler.add_header("ZenInfo", "images/zencode.jpg")
     if page.exists():
