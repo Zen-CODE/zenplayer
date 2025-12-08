b0ebae01dc822bf98bb9c14c2e19b7457694db85
Richard Larkin <richardla@discovery.co.za>
2025-12-08 12:06:25 +0200

    Add support for database extraction for PDF

--- zenstream/handlers/pdfviewer.py
+++ zenstream/handlers/pdfviewer.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from os.path import sep
+import tabula
+import pandas as pd
+from typing import List
 
 
 class PDFViewer:
@@ -11,3 +14,11 @@ class PDFViewer:
             lines = f.read()
 
         st.pdf(lines)
+
+        dataframes: List[pd.DataFrame] = tabula.read_pdf(file_name)
+        if len(dataframes) > 0:
+            for index, df in enumerate(dataframes):
+                with st.expander(f"Dataframe {index + 1}"):
+                    st.data_editor(df, num_rows="dynamic")
+        else:
+            st.info("No tables found in this PDF.")
