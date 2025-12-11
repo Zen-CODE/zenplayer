ea844f564d29b1bc9bca88d9d4c3215a97db13de
Richard Larkin <zenkey.zencode@gmail.com>
2025-12-11 03:33:44 +0200

    Start with structure

--- zenplayerlit/zenplayerlit/app_pages/musiclib.py
+++ zenplayerlit/zenplayerlit/app_pages/musiclib.py
@@ -20,10 +20,40 @@ class LibAnalysis:
                 for j, track in enumerate(library.get_tracks(artist, album)):
                     file_list.append(sep.join([album_path, track]))
 
+    def get_metadata(self) -> dict:
+        """Return a dictionary of metadata on the library.
+
+        Sample:
+        ```json
+        {
+            "Artists": 100,
+            "Albums": 100,
+            "Tracks": 100,
+            "Covers": 100
+        }
+        ```
+        """
+        return {"Artists": 100, "Albums": 100, "Tracks": 100, "Covers": 100}
+
 
 def show_musiclib():
+    col1, col2 = st.columns([0.8, 0.2])
+    with col2:
+        st.image("images/zencode.jpg", width=128)
+    with col1:
+        st.markdown("# Zen Library Info")
+    st.divider()
+
+    st.subheader("Library Metadata")
+
     with st.spinner("Loading Library..."):
         analysis = LibAnalysis()
 
         assert analysis
+
+        col1, col2 = st.columns([0.25, 0.75])
+        for name, value in analysis.get_metadata().items():
+            col1.markdown(f"**{name}**")
+            col2.write(value)
+
         st.write("Library loaded")
