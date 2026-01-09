772fdeef6b5d82179b669b95ddf70cd65889691e
Richard Larkin <zenkey.zencode@gmail.com>
2026-01-09 16:50:59 +0200

    :sparkles: Refactored

--- zenplayerlit/zenplayerlit/app_pages/zenplayer.py
+++ zenplayerlit/zenplayerlit/app_pages/zenplayer.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import requests
-from zencore import ZENPLAYER, ZENPLAYER_URL
+from zencore import ZENPLAYER_URL
 from styler import Styler
+from uuid import uuid4
 
 
-class ControlButtons:
-    """This class houses the control buttons for the media player."""
+class ZenPlayer:
+    def __init__(self):
+        self.data = {}
 
     @staticmethod
     def _button(name=None):
@@ -13,60 +15,54 @@ class ControlButtons:
         if name:
             requests.get(f"{ZENPLAYER_URL}/zenplayer/{name}")
 
-    @staticmethod
-    def show():
+    def show_control_buttons(self):
         """Adds a row of control buttons to the Streamlit app."""
         prev_, stop_, play_pause_, next_, vol_down_, vol_up_, refresh_ = st.columns(
             spec=[1, 1, 2, 1, 1, 1, 1], border=True
         )
         prev_.button(
             "⏮️",
-            on_click=ControlButtons._button,
+            on_click=self._button,
             args=("play_previous",),
             width="stretch",
         )
         stop_.button(
             "⏹️",
-            on_click=ControlButtons._button,
+            on_click=self._button,
             args=("stop",),
             width="stretch",
         )
         play_pause_.button(
             "▶️⏸️",
-            on_click=ControlButtons._button,
+            on_click=self._button,
             args=("play_pause",),
             width="stretch",
         )
         next_.button(
             "⏭️",
-            on_click=ControlButtons._button,
+            on_click=self._button,
             args=("play_next",),
             width="stretch",
         )
         vol_down_.button(
             "🔉",
-            on_click=ControlButtons._button,
+            on_click=self._button,
             args=("volume_down",),
             width="stretch",
         )
         vol_up_.button(
             "🔊",
-            on_click=ControlButtons._button,
+            on_click=self._button,
             args=("volume_up",),
             width="stretch",
         )
         refresh_.button(
             "⟳",
-            on_click=ControlButtons._button,
+            on_click=self._button,
             width="stretch",
         )
 
-
-class CoverImage:
-    """This class handles the display of the cover image for the media player."""
-
-    @staticmethod
-    def show():
+    def show_cover_image(self):
         def get_time(time_s):
             return (
                 str(int(time_s / 60)).zfill(2)
@@ -75,9 +71,9 @@ class CoverImage:
                 + "s"
             )
 
-        data = ZENPLAYER["data"]
+        data = self.data
         meta = requests.get(f"{ZENPLAYER_URL}/zenplayer/get_track_meta").json()
-        st.image(f"{ZENPLAYER_URL}/zenplayer/get_track_cover")
+        st.image(f"{ZENPLAYER_URL}/zenplayer/get_track_cover?random={uuid4()}")
         st.markdown(
             f"**{data['artist']}: {data['album']}** - "
             f"*{data['file_name'].split('/')[-1].split('.')[0]}*"
@@ -88,43 +84,34 @@ class CoverImage:
 
         st.write()
 
-
-class ProgressBar:
-    @staticmethod
-    def show():
-        st.progress(ZENPLAYER["data"]["position"], text=None, width="stretch")
-
-
-class Playlist:
-    @staticmethod
-    def show():
-        Playlist.update()
+    def show_progress_bar(self):
+        st.progress(self.data["position"], text=None, width="stretch")
 
     @staticmethod
-    def update():
+    def show_playlist():
         data = requests.get(f"{ZENPLAYER_URL}/zenplaylist/get_playlist").json()
         st.subheader("Playlist")
         st.divider()
         for item in data:
             st.write(item["text"])
 
-
-class ZenPlayer:
-    @staticmethod
-    def show():
-        Styler.add_header("ZenPlayer", "images/zencode.jpg")
-        ControlButtons.show()
-        st.divider()
+    @st.fragment(run_every=5)
+    def show_fragment(self):
+        self.data = requests.get(f"{ZENPLAYER_URL}/zenplayer/get_state").json()
 
         col1, col2 = st.columns(2)
         with col1:
-            CoverImage.show()
-            ProgressBar.show()
+            self.show_cover_image()
+            self.show_progress_bar()
         with col2:
-            Playlist.show()
+            self.show_playlist()
+
+    def show(self):
+        Styler.add_header("ZenPlayer", "images/zencode.jpg")
+        self.show_control_buttons()
+        st.divider()
+        self.show_fragment()
 
 
-@st.fragment(run_every=5)
 def show_zenplayer():
-    ZENPLAYER["data"] = requests.get(f"{ZENPLAYER_URL}/zenplayer/get_state").json()
-    ZenPlayer.sh
+    ZenPlayer().show()
